dbff377ff9fce6b2eea7b3070991cb0f2712c99b
Jeremy Tomasi <jeremy.tomasi@icloud.com>
2022-12-31 01:08:39 +0100

    config(nvim): 'lspconfig' plugin added

---
+++ config/nvim/after/plugin/lspconfig.lua
@@ -0,0 +1,50 @@
+local status, lspconfig = pcall(require,"lspconfig")
+if (not status) then return end
+
+local protocol = require('vim.lsp.protocol')
+
+local augroup_format = vim.api.nvim_create_augroup("Format", {clear = true})
+local enable_format_on_save = function(_, bufnr)
+  vim.api.nvim_clear_autocmds({group = augroup_format, buffer = bufnr})
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup_format,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({bufnr = bufnr})
+    end,
+  })
+end
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr,...) end
+
+  local opts = { noremap = true, silent = true }
+end
+
+protocol.CompletionItemKind = {
+  '', -- Text
+  '', -- Method
+  '', -- Function
+  '', -- Constructor
+  '', -- Field
+  '', -- Variable
+  '', -- Class
+  'ﰮ', -- Interface
+  '', -- Module
+  '', -- Property
+  '', -- Unit
+  '', -- Value
+  '', -- Enum
+  '', -- Keyword
+  '﬌', -- Snippet
+  '', -- Color
+  '', -- File
+  '', -- Reference
+  '', -- Folder
+  '', -- EnumMember
+  '', -- Constant
+  '', -- Struct
+  '', -- Event
+  'ﬦ', -- Operator
+  '', -- TypeParameter
+}
--- config/nvim/lua/jeremytmsi/plugins.lua
+++ config/nvim/lua/jeremytmsi/plugins.lua
@@ -2,5 +2,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
  use 'wbthomason/packer.nvim'
- use 'arcticicestudio/nord-vim'
+
+ use 'arcticicestudio/nord-vim' -- Theme
+
+ use 'neovim/nvim-lspconfig'
 end)
