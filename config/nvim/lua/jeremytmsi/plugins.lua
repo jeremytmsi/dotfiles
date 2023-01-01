15bfcd8ff56552779d74e492b0fd10430aa8811b
Jeremy Tomasi <jeremy.tomasi@icloud.com>
2023-01-01 18:27:07 +0100

    config(nvim): 'lualine' plugin added

--- config/nvim/lua/jeremytmsi/plugins.lua
+++ config/nvim/lua/jeremytmsi/plugins.lua
@@ -10,4 +10,9 @@ return require('packer').startup(function(use)
  use 'glepnir/lspsaga.nvim'
 
  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+
+ use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+}
 end)
